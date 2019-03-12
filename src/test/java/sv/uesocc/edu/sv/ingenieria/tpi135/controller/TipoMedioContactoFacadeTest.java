a15eb85d115185ec89d5aa6c28a50ef8637e0119
gochez <nhgochez95@gmail.com>
2019-03-12 13:16:10 -0600

    Test findALL de TipoMedioContacto

--- src/test/java/sv/uesocc/edu/sv/ingenieria/tpi135/controller/TipoMedioContactoFacadeTest.java
+++ src/test/java/sv/uesocc/edu/sv/ingenieria/tpi135/controller/TipoMedioContactoFacadeTest.java
@@ -5,21 +5,29 @@
  */
 package sv.uesocc.edu.sv.ingenieria.tpi135.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.embeddable.EJBContainer;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.mockito.runners.MockitoJUnitRunner;
 import sv.uesocc.edu.sv.ingenieria.tpi135.entity.TipoMedioContacto;
 
 /**
  *
- * @author gochez
+ * @author gochez //
  */
 public class TipoMedioContactoFacadeTest {
 
@@ -36,6 +44,7 @@ public class TipoMedioContactoFacadeTest {
 
     @Before
     public void setUp() {
+
     }
 
     @After
@@ -97,7 +106,7 @@ public class TipoMedioContactoFacadeTest {
         cut.em = em;
         Mockito.when(em.find(TipoMedioContacto.class, 1)).thenReturn(new TipoMedioContacto(1));
         TipoMedioContacto esperado = cut.find(1);
-        System.out.println("hola"+esperado.toString());
+        System.out.println("hola" + esperado.toString());
         assertEquals(new TipoMedioContacto(1), esperado);
 
     }
@@ -105,18 +114,24 @@ public class TipoMedioContactoFacadeTest {
     /**
      * Test of findAll method, of class TipoMedioContactoFacade.
      */
-//    @Test
-//    public void testFindAll() throws Exception {
-//        System.out.println("findAll");
-//        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-//        TipoMedioContactoFacadeLocal instance = (TipoMedioContactoFacadeLocal) container.getContext().lookup("java:global/classes/TipoMedioContactoFacade");
-//        List<TipoMedioContacto> expResult = null;
-//        List<TipoMedioContacto> result = instance.findAll();
-//        assertEquals(expResult, result);
-//        container.close();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    @Test
+    public void testFindAll() throws Exception {
+        System.out.println("findAll");
+        EntityManager em = Mockito.mock(EntityManager.class);
+        javax.persistence.criteria.CriteriaQuery cq = Mockito.mock(javax.persistence.criteria.CriteriaQuery.class);
+        TypedQuery tq = Mockito.mock(TypedQuery.class);
+        CriteriaBuilder cbr = Mockito.mock(CriteriaBuilder.class);
+        Mockito.when(em.getCriteriaBuilder()).thenReturn(cbr);
+        Mockito.when(em.createQuery(cq)).thenReturn(tq);
+        Mockito.when(em.getCriteriaBuilder().createQuery()).thenReturn(cq);
+        Mockito.when(em.createQuery(cq).getResultList()).thenReturn(new ArrayList());
+        TipoMedioContactoFacade cut = new TipoMedioContactoFacade();
+        cut.em = em;
+        int expResult = 0;
+        int esperado = cut.findAll().size();
+
+        assertEquals(expResult, esperado);
+    }
 //
 //    /**
 //     * Test of findRange method, of class TipoMedioContactoFacade.
@@ -138,16 +153,30 @@ public class TipoMedioContactoFacadeTest {
 //    /**
 //     * Test of count method, of class TipoMedioContactoFacade.
 //     */
-//    @Test
-//    public void testCount() throws Exception {
-//        System.out.println("count");
-//        EJBContainer container = javax.ejb.embeddable.EJBContainer.createEJBContainer();
-//        TipoMedioContactoFacadeLocal instance = (TipoMedioContactoFacadeLocal)container.getContext().lookup("java:global/classes/TipoMedioContactoFacade");
-//        int expResult = 0;
-//        int result = instance.count();
-//        assertEquals(expResult, result);
-//        container.close();
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+
+    @Test
+    public void testCount() throws Exception {
+        System.out.println("count");
+        EntityManager em = Mockito.mock(EntityManager.class);
+//        javax.persistence.criteria.CriteriaQuery cq = Mockito.mock(javax.persistence.criteria.CriteriaQuery.class);
+//        javax.persistence.criteria.Root rt = Mockito.mock(javax.persistence.criteria.Root.class);
+//        javax.persistence.Query q= Mockito.mock(javax.persistence.Query.class);
+//        CriteriaBuilder cb = Mockito.mock(CriteriaBuilder.class);
+//        TypedQuery tq = Mockito.mock(TypedQuery.class);
+//        Mockito.when(em.getCriteriaBuilder()).thenReturn(cb);
+//        Mockito.when(em.createQuery(cq)).thenReturn(tq);
+//        Mockito.when(em.getCriteriaBuilder().count(rt)).thenReturn(new Long("3"));
+       
+        Query q = Mockito.mock(Query.class);
+        Mockito.when(em.createQuery("SELECT COUNT(tmc.idTipoMedioContacto) FROM TipoMedioContacto tmc")).thenReturn(q);
+        Mockito.when(q.getSingleResult()).thenReturn(new Long(0));
+        TipoMedioContactoFacade facadeMock = new TipoMedioContactoFacade();
+        facadeMock.em= em;
+        int resultado= 0;
+        int esperado= facadeMock.count();
+        assertEquals( resultado, esperado);
+        
+        
+        
+    }
 }
