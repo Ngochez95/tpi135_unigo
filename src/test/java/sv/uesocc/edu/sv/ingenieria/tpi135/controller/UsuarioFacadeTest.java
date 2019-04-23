ee4b912bc16ab514a8c4b12f91a5ed346be25495
gochez <nhgochez95@gmail.com>
2019-04-23 13:36:07 -0600

    test findbyrol

--- src/test/java/sv/uesocc/edu/sv/ingenieria/tpi135/controller/UsuarioFacadeTest.java
+++ src/test/java/sv/uesocc/edu/sv/ingenieria/tpi135/controller/UsuarioFacadeTest.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.embeddable.EJBContainer;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import org.junit.After;
@@ -17,8 +18,11 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.internal.util.reflection.Whitebox;
 import sv.uesocc.edu.sv.ingenieria.tpi135.entity.Usuario;
+import sv.uesocc.edu.sv.ingenieria.tpi135.entity.Vehiculo;
 
 /**
  *
@@ -35,7 +39,23 @@ public class UsuarioFacadeTest extends AbstractFacadeT<Usuario> {
     protected Usuario getEntity() {
         return new Usuario(1);
     }
-    
-    
-    
+
+    @Mock
+    private EntityManager em;
+
+    @Before
+    public void inicializar() {
+        List<Vehiculo> ls = new ArrayList<>();
+        Query q = Mockito.mock(Query.class);
+        Mockito.when(em.createNamedQuery("Usuario.findByRol")).thenReturn(q);
+        Mockito.when(q.getResultList()).thenReturn(ls);
+    }
+
+    @Test
+    public void testFindByRol() {
+        UsuarioFacade cut = new UsuarioFacade();
+        Whitebox.setInternalState(cut, "em", em);
+        assertEquals(new ArrayList(), cut.findByRol());
+    }
+
 }
