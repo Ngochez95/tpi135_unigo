3770a751b841099572c874710bc22bdd57821a30
Ngochez95 <nhgochez95@gmail.com>
2019-05-25 01:31:51 -0600

    Implementando rest no generico para entidades,trayectoria, reserva y caracteristica

--- src/main/java/sv/uesocc/edu/sv/ingenieria/tpi135/boundary/rest/CaracteristicaResource.java
+++ src/main/java/sv/uesocc/edu/sv/ingenieria/tpi135/boundary/rest/CaracteristicaResource.java
@@ -6,12 +6,25 @@
 package sv.uesocc.edu.sv.ingenieria.tpi135.boundary.rest;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import sv.uesocc.edu.sv.ingenieria.tpi135.controller.AbstractFacade;
 import sv.uesocc.edu.sv.ingenieria.tpi135.controller.CaractetisticaFacade;
+import sv.uesocc.edu.sv.ingenieria.tpi135.controller.TipoCaracteristicaFacade;
 import sv.uesocc.edu.sv.ingenieria.tpi135.entity.Caractetistica;
+import sv.uesocc.edu.sv.ingenieria.tpi135.entity.TipoCaracteristica;
 
 /**
  *
@@ -19,23 +32,183 @@ import sv.uesocc.edu.sv.ingenieria.tpi135.entity.Caractetistica;
  */
 @Path("caracteristica")
 @RequestScoped
-public class CaracteristicaResource extends AbstractResource<Caractetistica> implements Serializable{
+public class CaracteristicaResource {
 
-   @Inject
-   protected CaractetisticaFacade caracteristica;
+    @Inject
+    protected CaractetisticaFacade caracteristica;
+    @Inject
+    protected TipoCaracteristicaFacade tipocaracteristica;
 
-  public CaracteristicaResource() {
-        super(Caractetistica.class
-        );
+    @GET
+    @Path("all")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public List<Caractetistica> CaractetisticafindAll() {
+        if (caracteristica != null) {
+            try {
+                List<Caractetistica> list = null;
+                list = caracteristica.findAll();
+                if (list != null) {
+                    return list;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
+        return new ArrayList<>();
     }
-    @Override
-    protected AbstractFacade<Caractetistica> getFacade() {
-        return caracteristica;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public List<Caractetistica> CaractetisticafindRange(@DefaultValue("0") @QueryParam("first") int first,
+            @DefaultValue("10") @QueryParam("pagesize") int pageSize) {
+
+        if (caracteristica != null && first >= 0 && pageSize >= 0) {
+            try {
+                List<Caractetistica> list = null;
+                list = caracteristica.findRange(first, pageSize);
+                if (list != null) {
+                    return list;
+                }
+
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
+        return new ArrayList<>();
+
     }
 
-    @Override
-    protected Caractetistica crearNuevo() {
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Integer Caractetisticacount() {
+        if (caracteristica != null) {
+            try {
+                return caracteristica.count();
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
+        return 0;
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Caractetistica CaractetisticafindById(
+            @PathParam("id") Integer id
+    ) throws InstantiationException, IllegalAccessException {
+        if (caracteristica != null) {
+            try {
+                Caractetistica registro;
+                List<Caractetistica> list = null;
+                if (id >= 0 && id != null) {
+                    registro = caracteristica.find(id);
+                    return registro;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
         return new Caractetistica();
     }
-    
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Caractetistica Caractetisticacreate(Caractetistica registro) throws InstantiationException, IllegalAccessException {
+        if (caracteristica != null && registro != null) {
+            try {
+                Caractetistica creado = caracteristica.create(registro);
+                if (creado != null) {
+                    return creado;
+                }
+
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
+        return new Caractetistica();
+    }
+
+    @GET
+    @Path("/caracteristica/tipocaracteristica/all")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public List<TipoCaracteristica> TipoCaracteristicafindAll() {
+        if (tipocaracteristica != null) {
+            try {
+                List<TipoCaracteristica> list = null;
+                list = tipocaracteristica.findAll();
+                if (list != null) {
+                    return list;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    @GET
+    @Path("/tipocaracteristica")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public List<TipoCaracteristica> TipoCaracteristicafindRange(@DefaultValue("0") @QueryParam("first") int first,
+            @DefaultValue("10") @QueryParam("pagesize") int pageSize) {
+
+        if (tipocaracteristica != null && first >= 0 && pageSize >= 0) {
+            try {
+                List<TipoCaracteristica> list = null;
+                list = tipocaracteristica.findRange(first, pageSize);
+                if (list != null) {
+                    return list;
+                }
+
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
+        return new ArrayList<>();
+
+    }
+
+    @GET
+    @Path("/tipocaracteristica/count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Integer TipoCaracteristicacount() {
+        if (tipocaracteristica != null) {
+            try {
+                return tipocaracteristica.count();
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
+        return 0;
+    }
+
+    @GET
+    @Path("/tipocaracteristica/{id}")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public TipoCaracteristica TipoCaracteristicafindById(
+            @PathParam("id") Integer id
+    ) throws InstantiationException, IllegalAccessException {
+        if (tipocaracteristica != null) {
+            try {
+                TipoCaracteristica registro;
+                if (id >= 0 && id != null) {
+                    registro = tipocaracteristica.find(id);
+                    return registro;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
+        return new TipoCaracteristica();
+    }
+
 }
--- src/main/java/sv/uesocc/edu/sv/ingenieria/tpi135/boundary/rest/TrayectoriaResource.java
+++ src/main/java/sv/uesocc/edu/sv/ingenieria/tpi135/boundary/rest/TrayectoriaResource.java
@@ -6,10 +6,22 @@
 package sv.uesocc.edu.sv.ingenieria.tpi135.boundary.rest;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import sv.uesocc.edu.sv.ingenieria.tpi135.controller.AbstractFacade;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 import sv.uesocc.edu.sv.ingenieria.tpi135.controller.TrayectoriaFacade;
 import sv.uesocc.edu.sv.ingenieria.tpi135.entity.Trayectoria;
 
@@ -19,24 +31,141 @@ import sv.uesocc.edu.sv.ingenieria.tpi135.entity.Trayectoria;
  */
 @Path("trayectoria")
 @RequestScoped
-public class TrayectoriaResource extends AbstractResource<Trayectoria> implements Serializable {
+public class TrayectoriaResource {
 
     @Inject
     protected TrayectoriaFacade trayectoria;
 
-    public TrayectoriaResource() {
-        super(Trayectoria.class);
+    @GET
+    @Path("all")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public List<Trayectoria> findAll() {
+        if (trayectoria != null) {
+            try {
+                List<Trayectoria> list = null;
+                list = trayectoria.findAll();
+                if (list != null) {
+                    return list;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
+        return new ArrayList<>();
     }
 
-    @Override
-    protected AbstractFacade<Trayectoria> getFacade() {
-        return trayectoria;
+    @GET
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public List<Trayectoria> findRange(@DefaultValue("0") @QueryParam("first") int first,
+            @DefaultValue("10") @QueryParam("pagesize") int pageSize) {
+
+        if (trayectoria != null && first >= 0 && pageSize >= 0) {
+            try {
+                List<Trayectoria> list = null;
+                list = trayectoria.findRange(first, pageSize);
+                if (list != null) {
+                    return list;
+                }
+
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
+        return new ArrayList<>();
+
     }
 
-    @Override
-    protected Trayectoria crearNuevo() {
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Integer count() {
+        if (trayectoria != null) {
+            try {
+                return trayectoria.count();
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
+        return 0;
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Trayectoria findById(
+            @PathParam("id") Integer id
+    ) throws InstantiationException, IllegalAccessException {
+        if (trayectoria != null) {
+            try {
+                Trayectoria registro=null;
+                if (id >= 0 && id != null) {
+                    registro= trayectoria.find(id);
+                    return registro;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
         return new Trayectoria();
+    }
 
+    @POST
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Trayectoria create(Trayectoria registro) throws InstantiationException, IllegalAccessException {
+        if (trayectoria != null && registro != null) {
+            try {
+                Trayectoria creado = trayectoria.create(registro);
+                if (creado != null) {
+                    return creado;
+                }
+
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+
+            }
+        }
+        return new Trayectoria();
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Trayectoria edit(Trayectoria registro) {
+        if (registro != null && trayectoria != null) {
+            try {
+                Trayectoria modificado = trayectoria.edit(registro);
+                if (modificado != null) {
+                    return modificado;
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, ex.getMessage(), ex);
+            }
+        }
+        return new Trayectoria();
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Trayectoria delete(
+            @PathParam("id") Integer id
+    ) throws InstantiationException, IllegalAccessException {
+
+        if (trayectoria != null && id >= 0 && id != null) {
+            try {
+                Trayectoria buscado = trayectoria.find(id);
+                if (buscado != null) {
+                    if (trayectoria.remove(buscado)) {
+                        return buscado;
+                    }
+                }
+            } catch (Exception e) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, e.getMessage(), e);
+
+            }
+        }
+        return new Trayectoria();
     }
 
 }
