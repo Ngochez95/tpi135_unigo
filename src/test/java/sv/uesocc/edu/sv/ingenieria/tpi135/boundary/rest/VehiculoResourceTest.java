7d78f90df4cf1f8332f2e28a518c51d38100da94
Ngochez95 <nhgochez95@gmail.com>
2019-06-25 14:29:46 -0600

    prueba tpi

--- src/test/java/sv/uesocc/edu/sv/ingenieria/tpi135/boundary/rest/VehiculoResourceTest.java
+++ src/test/java/sv/uesocc/edu/sv/ingenieria/tpi135/boundary/rest/VehiculoResourceTest.java
@@ -44,7 +44,7 @@ public class VehiculoResourceTest {
    // public static InMemoryRestServer server;
 
     public Client cliente = ClientBuilder.newClient();
-    public WebTarget target = cliente.target("http://localhost:8080/tpi135_unigo/ws/vehiculo");
+    public WebTarget target = cliente.target("192.168.43.2:8080/uniGo-1.0-SNAPSHOT/ws/marca/1/modelo/1/vehiculo");
 
     @BeforeClass
     public static void setUpClass() {
@@ -69,9 +69,9 @@ public class VehiculoResourceTest {
      * Test of VehiculofindAll method, of class VehiculoResource.
      */
     @Test
-    public void testVehiculofindAll() {
+    public void testVehiculofind() {
         System.out.println("VehiculofindAll");
-        Response response = target.path("/all").request(MediaType.APPLICATION_JSON).get();
+        Response response = target.path("/").request(MediaType.APPLICATION_JSON).get();
         assertNotNull(response);
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
         System.out.println("La respuesta a la petición all de vehiculo es: " + response.getStatusInfo());
@@ -96,87 +96,79 @@ public class VehiculoResourceTest {
     /**
      * Test of Vehiculocount method, of class VehiculoResource.
      */
-    @Test
-    public void testVehiculocount() {
-        System.out.println("Vehiculocount");
-        Response response = target.path("/count").request(MediaType.TEXT_PLAIN).get();
-        assertNotNull(response);
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        System.out.println("La pretición count de vehiculo fue: " + response.getStatusInfo());
-
-    }
+//    @Test
+//    public void testVehiculocount() {
+//        System.out.println("Vehiculocount");
+//        Response response = target.path("/count").request(MediaType.TEXT_PLAIN).get();
+//        assertNotNull(response);
+//        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+//        System.out.println("La pretición count de vehiculo fue: " + response.getStatusInfo());
+//
+//    }
 
     /**
      * Test of VehiculofindById method, of class VehiculoResource.
      */
-    @Test
-    public void testVehiculofindById() throws Exception {
-        System.out.println("VehiculofindById");
-        int id = 1;
-        Response response = target.path("/" + id).request(MediaType.APPLICATION_JSON).get();
-        assertNotNull(response);
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        System.out.println("La respuesta a la petición findID de vehiculo fue: " + response.getStatusInfo());
-    }
+//    @Test
+//    public void testVehiculofindById() throws Exception {
+//        System.out.println("VehiculofindById");
+//        int id = 1;
+//        Response response = target.path("/" + id).request(MediaType.APPLICATION_JSON).get();
+//        assertNotNull(response);
+//        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+//        System.out.println("La respuesta a la petición findID de vehiculo fue: " + response.getStatusInfo());
+//    }
 
     /**
      * Test of Vehiculocreate method, of class VehiculoResource.
      */
-    @Test
-    public void testVehiculocreate() throws Exception {
-//        System.out.println("Vehiculocreate");
-//        Vehiculo vh = new Vehiculo(1);
-//        Response response = server.newRequest("/").request().buildPost(Entity.entity(vh, MediaType.APPLICATION_JSON)).invoke();
-//        assertNotNull(response);
-//        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-//        System.out.println("El valor de la peticion del create de Vehiculo es: " + response.getStatusInfo());
-    }
+   
 
     /**
      * Test of MarcafindAll method, of class VehiculoResource.
      */
-    @Test
-    public void testMarcafindAll() {
-        System.out.println("MarcaFindAll");
-        Response response = target.path("/marca/all").request(MediaType.APPLICATION_JSON).get();
-        assertNotNull(response);
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        System.out.println("La respuesta a la petición all de marca es: " + response.getStatusInfo());
-    }
+//    @Test
+//    public void testMarcafindAll() {
+//        System.out.println("MarcaFindAll");
+//        Response response = target.path("/marca/all").request(MediaType.APPLICATION_JSON).get();
+//        assertNotNull(response);
+//        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+//        System.out.println("La respuesta a la petición all de marca es: " + response.getStatusInfo());
+//    }
 
-    @Test
-    public void testMarcacount() {
-        System.out.println("MarcaCount");
-        Response response = target.path("/marca/count").request(MediaType.TEXT_PLAIN).get();
-        assertNotNull(response);
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        System.out.println("La pretición count de marca fue: " + response.getStatusInfo());
-    }
+//    @Test
+//    public void testMarcacount() {
+//        System.out.println("MarcaCount");
+//        Response response = target.path("/marca/count").request(MediaType.TEXT_PLAIN).get();
+//        assertNotNull(response);
+//        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+//        System.out.println("La pretición count de marca fue: " + response.getStatusInfo());
+//    }
 
     /**
      * Test of MarcafindById method, of class VehiculoResource.
      */
-    @Test
-    public void testMarcafindById() throws Exception {
-        System.out.println("MarcafindById");
-        String id = "a";
-        Response response = target.path("/marca/findbyname/" + id).request(MediaType.APPLICATION_JSON).get();
-        assertNotNull(response);
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        System.out.println("La respuesta a la petición findID de vehiculo fue: " + response.getStatusInfo());
-    }
+//    @Test
+//    public void testMarcafindById() throws Exception {
+//        System.out.println("MarcafindById");
+//        String id = "a";
+//        Response response = target.path("/marca/findbyname/" + id).request(MediaType.APPLICATION_JSON).get();
+//        assertNotNull(response);
+//        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+//        System.out.println("La respuesta a la petición findID de vehiculo fue: " + response.getStatusInfo());
+//    }
 
     /**
      * Test of ModelofindAll method, of class VehiculoResource.
      */
-    @Test
-    public void testModelofindAll() {
-       System.out.println("VehiculofindAll");
-        Response response = target.path("/marca/modelo/all").request(MediaType.APPLICATION_JSON).get();
-        assertNotNull(response);
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        System.out.println("La respuesta a la petición all de modelo es: " + response.getStatusInfo());
-    }
+//    @Test
+//    public void testModelofindAll() {
+//       System.out.println("VehiculofindAll");
+//        Response response = target.path("/marca/modelo/all").request(MediaType.APPLICATION_JSON).get();
+//        assertNotNull(response);
+//        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+//        System.out.println("La respuesta a la petición all de modelo es: " + response.getStatusInfo());
+//    }
 
     /**
      * Test of ModelofindRange method, of class VehiculoResource.
@@ -197,26 +189,26 @@ public class VehiculoResourceTest {
     /**
      * Test of Modelocount method, of class VehiculoResource.
      */
-    @Test
-    public void testModelocount() {
-       System.out.println("ModeloCount");
-        Response response = target.path("/marca/modelo/count").request(MediaType.TEXT_PLAIN).get();
-        assertNotNull(response);
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        System.out.println("La pretición count de modelo fue: " + response.getStatusInfo());
-    }
+//    @Test
+//    public void testModelocount() {
+//       System.out.println("ModeloCount");
+//        Response response = target.path("/marca/modelo/count").request(MediaType.TEXT_PLAIN).get();
+//        assertNotNull(response);
+//        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+//        System.out.println("La pretición count de modelo fue: " + response.getStatusInfo());
+//    }
 
     /**
      * Test of ModelofindById method, of class VehiculoResource.
      */
-    @Test
-    public void testModelofindById() throws Exception {
-        System.out.println("ModelofindById");
-        int id = 1;
-        Response response = target.path("/marca/modelo/" + id).request(MediaType.APPLICATION_JSON).get();
-        assertNotNull(response);
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        System.out.println("La respuesta a la petición findID de modelo fue: " + response.getStatusInfo());
-    }
+//    @Test
+//    public void testModelofindById() throws Exception {
+//        System.out.println("ModelofindById");
+//        int id = 1;
+//        Response response = target.path("/marca/modelo/" + id).request(MediaType.APPLICATION_JSON).get();
+//        assertNotNull(response);
+//        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+//        System.out.println("La respuesta a la petición findID de modelo fue: " + response.getStatusInfo());
+//    }
 
 }
